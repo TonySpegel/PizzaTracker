9063cea5103264bb7ea43f00dc1b6cfdc30b0019
Tony Spegel <TonySpegel@googlemail.com>
2019-02-04 23:49:14 +0100

    chore(test): update name of app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:spaeti_compass/main.dart';
+import 'package:pizza_tracker/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(PizzaTracker());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
