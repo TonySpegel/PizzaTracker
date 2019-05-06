0136c353680abb2d8a890c869afc5d4da7ef4ec7
Tony Spegel <tony.spegel@flyacts.com>
2019-05-06 18:19:25 +0200

    remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-// FireStore
-import 'package:cloud_firestore/cloud_firestore.dart';
 // Material Theme
 import 'package:flutter/material.dart';
-// Internationalization for Dates
-import 'package:intl/intl.dart';
 // Widgets
 import 'pizza_item_screen.dart';
 
