3e8dcd3fb2992261edf9477203a405d3d7f3636f
Tony Spegel <TonySpegel@googlemail.com>
2019-06-27 15:45:15 +0200

    feat(form): use MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 // Widgets
 import 'pizza_form/topping.dart';
+import 'pizza_form/PizzaType.dart';
 import 'pizza_item_screen.dart';
 
 import 'package:flutter/rendering.dart';
@@ -15,26 +16,29 @@ void main() {
 class PizzaTracker extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      builder: (context) => Topping(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(builder: (context) => Topping()),
+        ChangeNotifierProvider(builder: (context) => PizzaType()),
+      ],
       child: MaterialApp(
-      home: new PizzaItemScreen(),
-      theme: ThemeData(
-        accentColor: Colors.amber,
-        buttonColor: Colors.amber,
-        primaryColor: Colors.amber,
-        cursorColor: Colors.amber,
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.amber, width: 1.0),
+        home: PizzaItemScreen(),
+        theme: ThemeData(
+          accentColor: Colors.amber,
+          buttonColor: Colors.amber,
+          primaryColor: Colors.amber,
+          cursorColor: Colors.amber,
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.amber, width: 1.0),
+            ),
+            labelStyle: TextStyle(color: Colors.amber)
           ),
-          labelStyle: TextStyle(color: Colors.amber)
+          toggleableActiveColor: Colors.amber,
+          primarySwatch: Colors.amber
         ),
-        toggleableActiveColor: Colors.amber,
-        primarySwatch: Colors.amber
       ),
-    )
     );
   }
 }
